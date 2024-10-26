a4633a59c41b7ecd8b683860616d4b6f307b5e78
fgalarzad <i202330589@cibertec.edu.pe>
2024-10-26 07:51:28 -0500

    arreglo

--- src/test/java/com/cibertec/DerpApplicationTests.java
+++ src/test/java/com/cibertec/DerpApplicationTests.java
@@ -3,7 +3,7 @@ package com.cibertec;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+@ActiveProfiles("test")
 @SpringBootTest
 class DerpApplicationTests {
 
