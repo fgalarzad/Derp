4c3bf1e245519d2bcb188f7c8b26c4f97ed10577
Limonedi <ecorie@unsa.edu.pe>
2024-10-26 00:28:51 -0500

    login editado

--- src/main/java/com/cibertec/controller/LoginController.java
+++ src/main/java/com/cibertec/controller/LoginController.java
@@ -1,16 +1,12 @@
 package com.cibertec.controller;
 
-import com.cibertec.model.Producto;
 import com.cibertec.model.Usuario;
 import com.cibertec.repository.UsuarioRepository;
-import com.cibertec.service.ProductoService;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
--- src/main/java/com/cibertec/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cibertec.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto,Integer>{
-
-}
